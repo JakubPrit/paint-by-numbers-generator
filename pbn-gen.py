dcecccdc8b4cf2035d3796650436ea0bea8b0858
Jakub Prítrský <jakub.prit@gmail.com>
2024-08-29 19:14:09 +0200

    wip

--- pbn-gen.py
+++ pbn-gen.py
@@ -10,14 +10,47 @@ from sklearn.cluster import KMeans # type: ignore
 #                  TYPE DEFINITIONS AND CONSTANTS                 #
 ###################################################################
 
-# todo
+Img = np.ndarray
+Pixel = Color = tp.Tuple[int, int, int]
+Pixels = np.ndarray
+
+
+###################################################################
+#                         COLOR CLUSTERING                        #
+###################################################################
+
+def cluster_colors(image: Img, n_colors: int, seed: int = 0) -> np.ndarray:
+    """ Cluster the colors of an image using KMeans.
+
+        Args:
+            image (Img): Image to cluster.
+            n_colors (int): Number of colors to cluster the image into.
+            seed (int, optional): Random seed for the KMeans algorithm. Defaults to 0.
+
+        Returns:
+            np.ndarray: Array of RGB colors.
+    """
+
+    pixels = image.reshape(-1, 3)
+    kmeans = KMeans(n_clusters=n_colors, random_state=seed).fit(pixels)
+    colors = kmeans.cluster_centers_
+    return colors
 
 
 ###################################################################
 #                     IMAGE LOADING AND SAVING                    #
 ###################################################################
 
-# todo
+def load_image(path: str) -> np.ndarray:
+    """Load an image from a file."""
+
+    return cv.imread(path)
+
+
+def save_image(path: str, image: np.ndarray) -> None:
+    """Save an image to a file."""
+
+    cv.imwrite(path, image)
 
 
 ###################################################################
@@ -25,11 +58,11 @@ from sklearn.cluster import KMeans # type: ignore
 ###################################################################
 
 
-def arg_parser() -> ArgumentParser:
+def _arg_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument('input',
-                        type=str,
-                        help='Path of input image.'
+                        type=str, nargs='+',
+                        help='Paths of input images.'
     )
     parser.add_argument('-o', '--output',
                         type=str, required=True,
@@ -37,7 +70,7 @@ def arg_parser() -> ArgumentParser:
                               The file extension is ignored and set to .png'
     )
     parser.add_argument(
-        '-n', '--color-palette-size',
+        '-k', '--color-palette-size',
         type=int, required=False,
         default=10,
         help='Number of colors used in the output. Defaults to 10.'
@@ -61,9 +94,12 @@ def arg_parser() -> ArgumentParser:
 
 
 def main() -> None:
-    args = arg_parser().parse_args()
+    args = _arg_parser().parse_args()
     print(args) # debug
-    # todo
+    for path in args.input:
+        image = load_image(path)
+        colors = cluster_colors(image, args.color_palette_size)
+        print(colors) # debug
 
 
 if __name__ == "__main__":
